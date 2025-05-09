2d2c419ab660f89c3cb8399c5c83d487828eb396
Marcelino <marcelinokenny12@gmail.com>
2025-05-09 16:58:03 +0700

    Add Details page for Resep with information and visualization components

---
+++ src/app/dashboard/resep/[id]/page.tsx
@@ -0,0 +1,107 @@
+import React from "react";
+import DashboardLayout from "@/app/layout/DashboardLayout";
+import CDNPieChart from "@/app/components/Chart";
+export default function Details() {
+  return (
+    <DashboardLayout>
+      <div>
+        <h2 className="text-2xl font-semibold text-black">
+          Detail Resep
+        </h2>
+        <div className="bg-[#CCE9C8] mt-10 rounded-2xl p-8 h-fit">
+          <div className="grid grid-cols-2 gap-4">
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col gap-4 h-fit bg-[#A9DBA4] rounded-2xl p-4">
+                <h2 className="text-[22px] font-semibold">Informasi Utama</h2>
+                <div className="grid grid-cols-[auto_min-content_1fr] gap-4 text-lg font-medium">
+                  <div>Nama Bahan</div>
+                  <div>:</div>
+                  <div>Beras</div>
+                  <div>Kategori</div>
+                  <div>:</div>
+                  <div>Diet</div>
+                  <div>Deskripsi</div>
+                  <div>:</div>
+                  <div>Nasi merah dengan ayam kukus</div>
+                  <div>Kategori Diet</div>
+                  <div>:</div>
+                  <ol className="list-decimal list-inside space-y-1">
+                    <li>Nasi Merah - 100g</li>
+                    <li>Ayam Kukus - 100g</li>
+                  </ol>
+                </div>
+              </div>
+              <div className="flex flex-col gap-4 h-fit bg-[#A9DBA4] rounded-2xl p-4">
+                <h2 className="text-[22px] font-semibold">Distribusi Utama</h2>
+                <div className="grid grid-cols-3 gap-4 text-lg font-medium">
+                  <div className="flex flex-row items-center gap-2">
+                    <div className="size-5 border rounded-md bg-[#F28B82]"></div>
+                    <h2>Kalori (7,5gr)</h2>
+                  </div>
+                  <div className="flex flex-row items-center gap-2">
+                    <div className="size-5 border rounded-md bg-[#F28B82]"></div>
+                    <h2>Karbohidrat (7,5gr)</h2>
+                  </div>
+                  <div className="flex flex-row items-center gap-2">
+                    <div className="size-5 border rounded-md bg-[#F28B82]"></div>
+                    <h2>Protein (7,5gr)</h2>
+                  </div>
+                  <div className="flex flex-row items-center gap-2">
+                    <div className="size-5 border rounded-md bg-[#F28B82]"></div>
+                    <h2>Lemak (7,5gr)</h2>
+                  </div>
+                  <div className="flex flex-row items-center gap-2">
+                    <div className="size-5 border rounded-md bg-[#F28B82]"></div>
+                    <h2>Serat (7,5gr)</h2>
+                  </div>
+                  <div className="flex flex-row items-center gap-2">
+                    <div className="size-5 border rounded-md bg-[#F28B82]"></div>
+                    <h2>Serat (7,5gr)</h2>
+                  </div>
+                  <div className="flex flex-row items-center gap-2">
+                    <div className="size-5 border rounded-md bg-[#F28B82]"></div>
+                    <h2>Serat (7,5gr)</h2>
+                  </div>
+                  <div className="flex flex-row items-center gap-2">
+                    <div className="size-5 border rounded-md bg-[#F28B82]"></div>
+                    <h2>Serat (7,5gr)</h2>
+                  </div>
+                  <div className="flex flex-row items-center gap-2">
+                    <div className="size-5 border rounded-md bg-[#F28B82]"></div>
+                    <h2>Serat (7,5gr)</h2>
+                  </div>
+                  <div className="flex flex-row items-center gap-2">
+                    <div className="size-5 border rounded-md bg-[#F28B82]"></div>
+                    <h2>Serat (7,5gr)</h2>
+                  </div>
+                  <div className="flex flex-row items-center gap-2">
+                    <div className="size-5 border rounded-md bg-[#F28B82]"></div>
+                    <h2>Serat (7,5gr)</h2>
+                  </div>
+                  <div className="flex flex-row items-center gap-2">
+                    <div className="size-5 border rounded-md bg-[#F28B82]"></div>
+                    <h2>Serat (7,5gr)</h2>
+                  </div>
+                  <div className="flex flex-row items-center gap-2">
+                    <div className="size-5 border rounded-md bg-[#F28B82]"></div>
+                    <h2>Serat (7,5gr)</h2>
+                  </div>
+                  <div className="flex flex-row items-center gap-2">
+                    <div className="size-5 border rounded-md bg-[#F28B82]"></div>
+                    <h2>Serat (7,5gr)</h2>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col gap-4 items-center h-full bg-[#A9DBA4] rounded-2xl p-4">
+              <h2 className="self-start text-[22px] font-semibold">
+                Visualisasi Distribusi
+              </h2>
+              <CDNPieChart />
+            </div>
+          </div>
+        </div>
+      </div>
+    </DashboardLayout>
+  );
+}
---
+++ src/app/dashboard/resep/page.tsx
@@ -0,0 +1,12 @@
+import DashboardLayout from "@/app/layout/DashboardLayout";
+import React from "react";
+
+export default function page() {
+  return (
+    <DashboardLayout>
+      <h2 className="text-2xl font-semibold text-black">
+        Resep Saya
+      </h2>
+    </DashboardLayout>
+  );
+}
